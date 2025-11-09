49d46706e14e274926f10222a1f4543e08746d8d
zasviat <zasviat@gmail.com>
2025-11-09 13:40:26 +0200

    error logging

--- main.py
+++ main.py
@@ -11,29 +11,36 @@ load_dotenv()
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 WEBHOOK_PATH = f"/webhook/{TOKEN}"          # Unique path for security
 WEBHOOK_URL = f"{os.environ['VERCEL_URL']}{WEBHOOK_PATH}"
-
+print(WEBHOOK_URL)
 
 app = FastAPI()
 
 
 @app.post(WEBHOOK_PATH)
 async def telegram_webhook(req: Request):
-    data = await req.json()
-    update = Update(**data)  # Convert JSON to Update object
-    await dp.feed_update(bot, update)  # now it works
-    return {"ok": True}
+    try:
+        data = await req.json()
+        update = Update(**data)  # Convert JSON to Update object
+        await dp.feed_update(bot, update)  # now it works
+        return {"ok": True}
+    except Exception as e:
+        import logging
+        logging.error(e)
 
 
 # Startup / shutdown events
 @app.on_event("startup")
 async def on_startup():
-    print("🤖 Setting webhook...")
-    await bot.delete_webhook()
-    await bot.set_webhook(WEBHOOK_URL)
-    
-    print("Configuring model...")
-    verify_model_exists(MODEL_PATH)
-    
+    try:
+        print("🤖 Setting webhook...")
+        await bot.delete_webhook()
+        await bot.set_webhook(WEBHOOK_URL)
+        
+        print("Configuring model...")
+        verify_model_exists(MODEL_PATH)
+    except Exception as e:
+        import logging
+        logging.error(e)
     # await main()
 
 
